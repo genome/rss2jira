f6a431015ce0de2341076ec0c480d7a9972eef6b
Nathan Nutter <iam@nnutter.com>
2012-09-20 17:00:16 -0500

    0.1.1: specifies install requirements

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 
 setup(
     name='rss2jira',
-    version='0.1',
+    version='0.1.1',
     description='Create JIRA issues from RSS entries matching key terms.',
     author='nnutter',
     author_email='iam@nnutter.com',
     url='https://github.com/nnutter/rss2jira',
     packages=['rss2jira'],
-    requires=['PyYAML (>3.10)', 'feedparser (>5.1.2)', 'jira (>0.12)'],
+    install_requires=['PyYAML (>3.10)', 'feedparser (>5.1.2)', 'jira (>0.12)'],
     scripts=['bin/rss2jira'],
     package_data={'rss2jira': ['rss2jira.conf.example']},
     data_files=[('/etc', ['rss2jira.conf.example'])],
