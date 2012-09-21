4088547332692d93eebeb9c69c4158a66db8dade
Nathan Nutter <iam@nnutter.com>
2012-09-21 14:14:17 -0500

    got requirements and data files working

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='rss2jira',
-    version='0.1.2',
+    version='0.1.6',
     description='Create JIRA issues from RSS entries matching key terms.',
     author='nnutter',
     author_email='iam@nnutter.com',
     url='https://github.com/nnutter/rss2jira',
     packages=['rss2jira'],
-    requires=['PyYAML (>3.10)', 'feedparser (>5.1.2)', 'jira (>0.12)'],
+    install_requires=['PyYAML>=3.10', 'feedparser>=5.1.2', 'jira-python>=0.12'],
     scripts=['bin/rss2jira'],
-    package_data={'rss2jira': ['rss2jira.conf.example']},
-    data_files=[('/etc', ['rss2jira.conf.example'])],
+    include_package_data=True,
     license='BSD',
     keywords=['rss', 'jira', 'rss2jira'],
     classifiers=[
