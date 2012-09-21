1ee2a29500452752b136ec14e47f29e545fc61a7
Nathan Nutter <iam@nnutter.com>
2012-09-21 15:24:43 -0500

    version bump to 0.1.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='rss2jira',
-    version='0.1.6',
+    version='0.1.7',
     description='Create JIRA issues from RSS entries matching key terms.',
     author='nnutter',
     author_email='iam@nnutter.com',
