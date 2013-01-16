4377a241b2f8da5fdfd19d097ef53aeb6d837d36
Nathan Nutter <iam@nnutter.com>
2013-01-16 14:01:36 -0600

    0.1.14 version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='rss2jira',
-    version='0.1.13',
+    version='0.1.14',
     description='Create JIRA issues from RSS entries matching key terms.',
     author='nnutter',
     author_email='iam@nnutter.com',
