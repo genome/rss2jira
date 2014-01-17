3251a69a829bdc9d73fe359e6092fad000acd8ce
Travis Abbott <tabbott@genome.wustl.edu>
2014-01-16 18:38:22 -0600

    add logging to sql backend

--- rss2jira/trackedentries.py
+++ rss2jira/trackedentries.py
@@ -1,18 +1,5 @@
+import logging
 import sqlite3
-
-class _MemoryTrackedEntries(object):
-    def __init__(self):
-        self.data = set()
-
-    def clear(self):
-        self.data.clear()
-
-    def add(self, source_name, identifier):
-        self.data.add((source_name, identifier))
-
-    def contains(self, source_name, identifier):
-        return (source_name, identifier) in self.data
-
 
 class Sqlite3TrackedEntries(object):
     _CREATE_ENTRY_TABLE_SQL = """
@@ -22,7 +9,7 @@ class Sqlite3TrackedEntries(object):
         PRIMARY KEY (source_name, id))
     """
 
-    _DROP_ENTRY_TABLE_SQL = "DROP TABLE IF EXISTS entries"
+    _CLEAR_ENTRY_TABLE_SQL = "DELETE FROM entries"
 
     _ENTRY_EXISTS_SQL = """
     SELECT COUNT(1) FROM entries
@@ -31,27 +18,50 @@ class Sqlite3TrackedEntries(object):
 
     _INSERT_ENTRY_SQL = "INSERT INTO entries VALUES ('{}', '{}')"
 
+    class _SourceView(object):
+        def __init__(self, source_name, tracker):
+            self.source_name = source_name
+            self.tracker = tracker
+
+        def __contains__(self, x):
+            return self.tracker.contains(self.source_name, x)
+
+        def add(self, x):
+            self.tracker.add(self.source_name, x)
+
     def __init__(self, db_path):
+        self.logger = logging.getLogger("rss2jira.SQL")
         self.db_path = db_path
         self.db = sqlite3.connect(self.db_path)
         self._create_tables()
 
     def _create_tables(self):
-        self.db.cursor().execute(self._CREATE_ENTRY_TABLE_SQL)
-
-    def _drop_tables(self):
-        self.db.cursor().execute(self._DROP_ENTRY_TABLE_SQL)
+        self.exec_sql(self._CREATE_ENTRY_TABLE_SQL)
+        self.db.commit()
 
     def clear(self):
-        self._drop_tables()
-        self._create_tables()
+        self.exec_sql(self._CLEAR_ENTRY_TABLE_SQL)
+        self.db.commit()
 
     def contains(self, source_name, identifier):
         sql = self._ENTRY_EXISTS_SQL.format(source_name, identifier)
-        result = self.db.cursor().execute(sql)
+        result = self.exec_sql(sql)
         return result.fetchone()[0] > 0
 
     def add(self, source_name, identifier):
         sql = self._INSERT_ENTRY_SQL.format(source_name, identifier)
-        self.db.cursor().execute(sql)
+        self.exec_sql(sql)
         self.db.commit()
+
+    def exec_sql(self, sql):
+        self.logger.debug(sql)
+        return self.db.cursor().execute(sql)
+
+    def source_view(self, source_name):
+        return self._SourceView(source_name, self)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.db.close()
